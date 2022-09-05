c390d9fb971c2a8bf252d8290e22c8ff9026f248
JuanjoCieri <juin@hotmail.com.ar>
2022-09-04 23:26:33 -0300

    Token hardcodeado fixed | Saque validaciones register user y pets

--- src/components/PrivateRoute.js/PrivateRoute.js
+++ src/components/PrivateRoute.js/PrivateRoute.js
@@ -4,8 +4,12 @@ const PrivateRoutes = () => {
   const tokenn = localStorage.getItem("token");
 
   let auth = { token: tokenn ? true : false };
+  
+  const pattern = /^([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_\-\+\/=]*)/gi.test(tokenn)
 
-  return auth.token ? <Outlet /> : <Navigate to="/" />;
+  console.log(pattern)
+
+  return auth.token && pattern ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default PrivateRoutes;
