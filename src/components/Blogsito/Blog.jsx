362fc31a4dd8a49be8f420e2d5d256df841a175f
JuanjoCieri <juin@hotmail.com.ar>
2022-09-05 17:23:10 -0300

    Blogs terminados

--- src/components/Blogsito/Blog.jsx
+++ src/components/Blogsito/Blog.jsx
@@ -103,13 +103,13 @@ export default function Blog() {
               <img alt="blog" src={paradonar} className="w-screen" />
             </a>
 
-            <a href="https://happytails.vercel.app/home">
+            <a href="https://happytails.vercel.app/blog/3">
               <img alt="blog" src={carpi} className="w-screen  " />
             </a>
-            <a href="https://happytails.vercel.app/home">
+            <a href="https://happytails.vercel.app/blog/2">
               <img alt="blog" src={amarillobaño} className="w-screen" />
             </a>
-            <a href="https://happytails.vercel.app/home">
+            <a href="https://happytails.vercel.app/blog/1">
               <img alt="blog" src={cachita} className="w-screen  " />
             </a>
           </Carousel>
