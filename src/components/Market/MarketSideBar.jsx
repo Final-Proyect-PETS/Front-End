49cfc4485e44e58b0630e1b3fbeb7e5fb03a983d
tablonimus <lautamartin1995@gmail.com>
2022-09-15 13:09:41 -0300

    compartir

--- src/components/Market/MarketSideBar.jsx
+++ src/components/Market/MarketSideBar.jsx
@@ -1,22 +1,17 @@
-import React from "react"
-import SearchBarProduct from "../SearchBars/SearchBarProduct.jsx"
+import React from "react";
+import SearchBarProduct from "../SearchBars/SearchBarProduct.jsx";
 
 export default function MarketSideBar({ setCurrentPage }) {
-
-
-    return (
-        <nav className="w-full h-full flex flex-col items-center justify-around bg-yellow-700">
-            <div>
-                <h1 className="text-2xl text-white font-semibold">MarketPlace</h1>
-            </div>
-            <div>
-                <SearchBarProduct
-                    setCurrentPage={setCurrentPage}
-                />
-            </div>
-            <div>
-                <h3>FILTROS</h3>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="w-screen  h-30 flex lg:flex-row pt-14 items-center justify-around bg-yellow-500">
+      <div className="flex flex-col lg:flex-row justify-center items-center">
+        <div className="pt-5">
+          <SearchBarProduct /* setCurrentPage={setCurrentPage} */ />
+        </div>
+      </div>
+      <div>
+        <h3>FILTROS</h3>
+      </div>
+    </nav>
+  );
 }
